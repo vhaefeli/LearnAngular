0a5b039fbcf64155625cf61840b0fdf866f09ea9
Vhaefeli <vhaefeli@student.42lausanne.ch>
2023-08-12 20:06:53 +0200

    les 3 premieres heures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //uniquement dans le composant racine. cela dit quel est le premier fichier a lancer en premier
 })
 export class AppModule { }
